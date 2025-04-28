e3896789dd4f0ac51e3b87e8d0838b1503fab855
Ihar Harokhau <harokhaui@gmail.com>
2025-04-28 15:40:48 +0300

    Added ready homework module3

---
+++ src/main/java/Module3/Main.java
@@ -0,0 +1,12 @@
+package Module3;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MyIOFile myIOFile = new MyIOFile();
+
+        myIOFile.inputFile();
+        myIOFile.outputFile();
+    }
+}
